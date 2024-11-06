cb1227b21855a5b976d9d7ea75d222d76fffa46b
talisviirpalu <talis.viirpalu@wisercat.net>
2024-11-06 19:42:44 +0200

    Initial Commit

---
+++ MusicCollab/MusicCollabApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicCollabApp.swift
+//  MusicCollab
+//
+//  Created by Talis Viirpalu on 06.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct MusicCollabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MusicCollabTests/MusicCollabTests.swift
@@ -0,0 +1,17 @@
+//
+//  MusicCollabTests.swift
+//  MusicCollabTests
+//
+//  Created by Talis Viirpalu on 06.11.2024.
+//
+
+import Testing
+@testable import MusicCollab
+
+struct MusicCollabTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
